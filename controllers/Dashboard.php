d4a2be4f0bfd03444678d23ae19c607680a00769
Arturo Ventura <artuvntu@icloud.com>
2020-10-17 17:58:41 -0500

    some change

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -3,19 +3,68 @@
 class Dashboard extends Tornado\Controller {
 
     public function home($req,$res)  {
+
+    	echo $this->renderView($res,[
+            "worstComments"=>$this->getWorstComments(),
+            "bestComments"=>$this->getBestComments(),
+            ]);
+    }
+    /**
+    * Get WorstCOmments
+    */
+    private function getWorstComments() {
         $array=[];
-    	$mapper=$this->spot->mapper("Entity\Tweet");
-    	$data=$mapper->query("select * from getTweetData")->toArray();
-    	foreach ($data as $key => $value) {
-    		$aux=[];
-    		$aux["query"]=str_replace("@","",$value["cuenta"]);
-    		$aux["promedioRT"]=$value["avg_rt"];
-    		$aux["promedioFV"]=$value["avg_fv"];
-    		$aux["maxRT"]=$value["max_rt"];
-    		$aux["maxFV"]=$value["max_fv"];
-    		$aux["fecha"]=$value["fecha"];
-    		array_push($array,$aux);
-    	}
-    	echo $this->renderView($res,["data"=>$array]);
+        $mapper=$this->spot->mapper("Entity\Tweet");
+        $banks=$mapper->query("select distinct cuenta from getTweetData")->toArray();
+        foreach ($banks as $key => $banco) {
+            $auxbanco=[];
+            $data=$mapper->query("select * from getWorstComments where vectorSentimiento<0 and cuenta='".$banco["cuenta"]."' limit 10")->toArray();
+            foreach ($data as $key => $value) {
+                $aux=[];
+                $aux["query"]=str_replace("@","",$value["cuenta"]);
+                $aux["vectorSentimiento"]=$value["vectorSentimiento"];
+                $aux["fecha"]=$value["fecha"];
+                $aux["texto"]=$value["texto"];
+                $aux["rt"]=$value["rt"];
+                $aux["fav"]=$value["fav"];
+                array_push($auxbanco,$aux);
+            }
+            $array=array_merge($array, [$banco["cuenta"]=>$auxbanco]);
+        }
+        
+        
+        // header('Content-Type: application/json');
+        // echo json_encode($array);
+        return $array;
+    }
+
+        /**
+    * Get BestComments
+    */
+    private function getBestComments() {
+        $array=[];
+        $mapper=$this->spot->mapper("Entity\Tweet");
+        $banks=$mapper->query("select distinct cuenta from getTweetData")->toArray();
+        foreach ($banks as $key => $banco) {
+            $auxbanco=[];
+            $data=$mapper->query("select * from getWorstComments where vectorSentimiento>0 and cuenta='".$banco["cuenta"]."' order by vectorSentimiento desc,rt desc,fav desc limit 10")->toArray();
+            foreach ($data as $key => $value) {
+                $aux=[];
+                $aux["query"]=str_replace("@","",$value["cuenta"]);
+                $aux["vectorSentimiento"]=$value["vectorSentimiento"];
+                $aux["fecha"]=$value["fecha"];
+                $aux["texto"]=$value["texto"];
+                $aux["rt"]=$value["rt"];
+                $aux["fav"]=$value["fav"];
+                array_push($auxbanco,$aux);
+            }
+            $array=array_merge($array, [$banco["cuenta"]=>$auxbanco]);
+
+        }
+        
+        
+        // header('Content-Type: application/json');
+        // echo json_encode($array);
+        return $array;
     }
 }
--- resources/js/modules/home.js
+++ resources/js/modules/home.js
@@ -1,5 +1,6 @@
 $(()=>{
 	configRetweetDiario()
+	configFavDiario()
 })
 var retweetDiarioChart
 function configRetweetDiario() {
@@ -36,47 +37,84 @@ function configRetweetDiario() {
 
 function updateRetweetDiario() {
 	fetch('/api/getMaxDataByDay').then(j=>j.json()).then(data => {
-		let bbvaData = data.filter(element=>element.query == 'bbva')
+		let bbvaData = data.bbva
 		let labels = bbvaData.map(element=>formatDate(new Date(element.fecha.date)));
-		let bbvaDataset = data.map(element=>element.promedioRT)
-		let bancos = []
-		data.forEach(element=>{
-			if (!bancos.includes(element.query)) {
-				bancos.push(element.query)
-			}
-		})
-		console.log(bancos);
+		let bbvaDataset = bbvaData.map(element=>element.promedioRT)
+		let datasets = Object.entries(data).map((element,index)=>({
+			label               : element[0],
+			backgroundColor     : index == 0 ? 'rgba(60,141,188,0.5)' : index == 1 ? 'rgba(188, 60, 141, 0.5)' : 'rgba(141, 188, 60, 0.5)',
+			borderColor         : index == 0 ? 'rgba(60,141,188,0.8)' : index == 1 ? 'rgba(188, 60, 141, 0.8)' : 'rgba(141, 188, 60, 0.8)',
+			data                : (labels.map(item=> {
+				let find = (element[1].find(data => formatDate(new Date(data.fecha.date)) == item)) 
+				return find ? find.promedioRT : 0
+			}))})
+		)
 		var areaChartData = {
 			labels,
-			datasets: [
-			{
-				label               : 'Digital Goods',
-				backgroundColor     : 'rgba(60,141,188,0.9)',
-				borderColor         : 'rgba(60,141,188,0.8)',
-				pointRadius          : false,
-				pointColor          : '#3b8bba',
-				pointStrokeColor    : 'rgba(60,141,188,1)',
-				pointHighlightFill  : '#fff',
-				pointHighlightStroke: 'rgba(60,141,188,1)',
-				data                : bbvaDataset
-			},
-			// {
-			// 	label               : 'Electronics',
-			// 	backgroundColor     : 'rgba(210, 214, 222, 1)',
-			// 	borderColor         : 'rgba(210, 214, 222, 1)',
-			// 	pointRadius         : false,
-			// 	pointColor          : 'rgba(210, 214, 222, 1)',
-			// 	pointStrokeColor    : '#c1c7d1',
-			// 	pointHighlightFill  : '#fff',
-			// 	pointHighlightStroke: 'rgba(220,220,220,1)',
-			// 	data                : [65, 59, 80, 81, 56, 55, 40]
-			// },
-			]
+			datasets
 		}
 		retweetDiarioChart.data = areaChartData
 		retweetDiarioChart.update()
 	})
 }
+
+var favDiarioChart
+function configFavDiario() {
+	var areaChartCanvas = $('#favDiario').get(0).getContext('2d')
+	var areaChartOptions = {
+		maintainAspectRatio : false,
+		responsive : true,
+		legend: {
+			display: false
+		},
+		scales: {
+			xAxes: [{
+				gridLines : {
+					display : false,
+				}
+			}],
+			yAxes: [{
+				gridLines : {
+					display : false,
+				}
+			}]
+		}
+	}
+
+    // This will get the first returned node in the jQuery collection.
+    favDiarioChart = new Chart(areaChartCanvas, {
+    	type: 'line',
+    	// data: areaChartData,
+    	options: areaChartOptions
+    })
+
+    updateFavDiario()
+}
+
+function updateFavDiario() {
+	fetch('/api/getMaxDataByDay').then(j=>j.json()).then(data => {
+		let bbvaData = data.bbva
+		let labels = bbvaData.map(element=>formatDate(new Date(element.fecha.date)));
+		let bbvaDataset = bbvaData.map(element=>element.promedioRT)
+		let datasets = Object.entries(data).map((element,index)=>({
+			label               : element[0],
+			backgroundColor     : index == 0 ? 'rgba(200,128,200,0.5)' : index == 1 ? 'rgba(0, 0, 259, 0.5)' : 'rgba(128, 128, 0, 0.5)',
+			borderColor         : index == 0 ? 'rgba(60,141,188,0.8)' : index == 1 ? 'rgba(188, 60, 141, 0.8)' : 'rgba(141, 188, 60, 0.8)',
+			data                : (labels.map(item=> {
+				let find = (element[1].find(data => formatDate(new Date(data.fecha.date)) == item)) 
+				console.log(find);
+				return find ? find.promedioRT : 0
+			}))})
+		)
+		var areaChartData = {
+			labels,
+			datasets
+		}
+		favDiarioChart.data = areaChartData
+		favDiarioChart.update()
+	})
+}
+
 
 function formatDate(date) {
 	return date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()
